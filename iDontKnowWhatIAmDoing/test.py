39ee2c4dd54225691d415f9366664709f8c1e41b
Sebastian Pelayo <z5491913@ad.unsw.edu.au>
2024-06-25 22:08:40 +1000

    a

--- iDontKnowWhatIAmDoing/test.py
+++ iDontKnowWhatIAmDoing/test.py
@@ -2,7 +2,11 @@
 import wfdb 
 import numpy
 path = "/home/sebastian/VIP/Mimic-Prep/SampleData/p10000032/s44458630/44458630"
-    
+study_num = path.split('/')[-1]  #get last study num
+graph_title = 'Study' + study_num
+
+
+print(study_num)
 rd_record = wfdb.rdrecord(path) 
 print(rd_record)
-wfdb.plot_wfdb(record=rd_record, figsize=(24,18), title='Study 41420867 example', ecg_grids='all')
+wfdb.plot_wfdb(record=rd_record, figsize=(24,18), title=graph_title, ecg_grids='all')
