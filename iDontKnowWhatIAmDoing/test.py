3842d580e9308774c7c79fc6f8e91aebd2406510
SebPelay0 <166897517+SebPelay0@users.noreply.github.com>
2024-06-26 14:35:35 +1000

    Revert "changed file names"
    This reverts commit 4ae61350e3f97035fabc016b6f7bfc69fe682696, reversing
    changes made to fe496230a29cd77f6c41d891de5b41333b52c39f.

---
+++ iDontKnowWhatIAmDoing/test.py
@@ -0,0 +1,65 @@
+
+import wfdb 
+
+from pathlib import Path
+import pandas as pd
+import datetime
+
+
+folderNum = 'p10000032'
+dataPath = r"/home/sebastian/VIP/Mimic-Prep/SampleData/"
+
+
+
+paths = list(Path(folderNum).rglob("*.hea"))
+studyData = {'studyNum':[],'date':[],'time':[]}
+studyDates = []
+studyTimes = []
+i = 0
+
+for file in paths:
+    pathObject = paths[i]
+    #Extract study number
+    studyNum = pathObject.name.split('.hea')[0]
+    studyData['studyNum'].append(studyNum)
+
+    #Extract date and times
+    study = file.stem
+    metadata = wfdb.rdheader(f'{file.parent}/{file.stem}')
+    studyDates.append(metadata.base_date)
+    studyTimes.append(metadata.base_time)
+
+
+    #get file path to produce image
+    filePath = (dataPath + str(pathObject)).split('.hea')[0]
+    print(filePath)
+    rd_record = wfdb.rdrecord(filePath) 
+
+    #save figure 
+    fig = wfdb.plot_wfdb(record=rd_record, figsize=(24,18), title= 'Study No.' + studyNum, ecg_grids='all', return_fig=True)
+    #pdf or png here? can just change file extension
+    fig.savefig(studyNum + ".png")
+
+    
+    i = i + 1
+
+
+j = 0
+for study in studyDates:
+    studyData['date'].append(studyDates[j].strftime('%Y-%m-%d'))
+    studyData['time'].append(studyTimes[j].strftime('%Y-%m-%d'))
+    j =j + 1
+
+
+print(studyData)
+
+
+# rd_record = wfdb.rdrecord(path) 
+# print(rd_record)
+# wfdb.plot_wfdb(record=rd_record, figsize=(24,18), title=graph_title, ecg_grids='all')
+
+
+
+
+# folderNum = 'p10000032'
+# paths = list(Path(folderNum).rglob("*.hea"))
