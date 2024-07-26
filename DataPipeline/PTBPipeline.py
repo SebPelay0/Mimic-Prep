1da1121ace333b77bdcff238c1d487f86fd1d106
Sebastian Pelayo <z5491913@ad.unsw.edu.au>
2024-07-26 17:17:26 +1000

    folder names

--- DataPipeline/PTBPipeline.py
+++ DataPipeline/PTBPipeline.py
@@ -2,10 +2,23 @@ import wfdb
 from pathlib import Path
 import csv
 import pandas as pd
+import os
 
+classes = ['NORM', 'IMI', 'NDT', 'ASMI', ' LVH', 'LAFB', 'IRBB', 'CLBBB', 'NST_:', 'CRBBB']
 # Define the directory containing the patient folders
 root_dir = Path(__file__).resolve().parent.parent
 dataPath = root_dir / "physionet.org/files"
+
+imagesPath = root_dir/'data'
+
+# make folder for each of the 10 SCP codes
+for label in classes:
+    class_path = os.path.join(imagesPath, label.strip())
+    if not (os.path.exists(class_path)):
+        os.makedirs(class_path)
+        print('made directory' + class_path)
+        
+
 
 # Use rglob to find the main CSV file
 csv_files = list(dataPath.rglob("ptbxl_database.csv"))[0]
@@ -23,9 +36,8 @@ with open(csv_files) as file_obj:
     filename_lr_index = headers.index("filename_lr")  # Get the index of filename_lr
 
     for row in reader_obj:
-        patient_id = row[1]
-        report = row[10]
-        scp_codes = row[11]
+        #save only SCP
+        scp_code = row[11]
         filename_lr = row[filename_lr_index]  # Extract the filename_lr
 
         # Define the path to the corresponding .hea file
@@ -41,8 +53,8 @@ with open(csv_files) as file_obj:
 
             # Read the raw ECG signal data
             # .hea files contain header information while .dat files and others contain the signal data
-            record = wfdb.rdrecord(str(hea_file_path.with_suffix("")))
-            ecg_data = record.p_signal
+            rd_record = wfdb.rdrecord(str(hea_file_path.with_suffix("")))
+            ecg_data = rd_record.p_signal
 
             # Print out the attributes of the record object [useful if you want to see whats inside the wfdb record]
             # attributes = [
@@ -53,23 +65,18 @@ with open(csv_files) as file_obj:
             # for attr in attributes:
             #     print(f"{attr}: {getattr(record, attr)}")
 
+            # row[1] indicates patientId
             # Append the data to the output structure
-            data_records.append(
-                {
-                    "study_num": study_num,
-                    "patient_id": patient_id,
-                    "report": report,
-                    "scp_codes": scp_codes,
-                    "ecg_data": ecg_data.tolist(),  # Convert numpy array to list for serialization
-                }
-            )
+            fig = wfdb.plot_wfdb(record=rd_record, figsize=(24, 18), title=row[1], ecg_grids='all', return_fig=True)
+            image = fig.savefig(row[1] + '.png')
+            print(image)
             break
         except Exception as e:
             print(f"Error processing file {hea_file_path}: {e}")
             continue
 
 # Save the collected data into a structured format
-output_file = root_dir / "ecg_data.json"
+output_file = root_dir /  "ecg_data.json"
 pd.DataFrame(data_records).to_json(output_file, orient="records")
 
 print(f"Data has been saved to {output_file}")
